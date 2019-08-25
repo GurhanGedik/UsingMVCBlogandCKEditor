b457aaa5c1f7fbfb830d74fe08fc314321b04602
Gurhan <gurhangedik@hotmail.com>
2019-08-26 02:04:58 +0300

    Update Data

--- MVCLoginPageAndCKEditor/MVCLoginPageAndCKEditor/Controllers/HomeController.cs
+++ MVCLoginPageAndCKEditor/MVCLoginPageAndCKEditor/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MVCLoginPageAndCKEditor.Models.Data.Context;
+using MVCLoginPageAndCKEditor.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,19 +10,35 @@ namespace MVCLoginPageAndCKEditor.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
             return View();
         }
+
         public ActionResult SignUp()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult SignUp(ApplicationUser user)
+        {
+            ProjectContext db = new ProjectContext();
+            ApplicationUser newUser = new ApplicationUser();
+            newUser.Name = user.Name;
+            newUser.Password = user.Password;
+            newUser.Email = user.Email;
+            db.User.Add(newUser);
+            db.SaveChanges();
+
+            return RedirectToAction("Index","Home");
+        }
+
         public ActionResult SignIn()
         {
             return View();
         }
+
         public ActionResult EnterBlog()
         {
             return View();
---
+++ MVCLoginPageAndCKEditor/MVCLoginPageAndCKEditor/Models/Data/Context/ProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MVCLoginPageAndCKEditor.Models.Entities;
+
+namespace MVCLoginPageAndCKEditor.Models.Data.Context
+{
+    public class ProjectContext:DbContext
+    {
+        public ProjectContext()
+        {
+            Database.Connection.ConnectionString = "Server=.\\SQLEXPRESS;Database=MyBlogDB;Integrated Security = True;";
+        }
+        public DbSet<ApplicationUser> User { get; set; }
+        public DbSet<Blogs> Blog { get; set; }
+    }
+}
